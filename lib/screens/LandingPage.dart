dcfb92f8ae12ad2ba0ebc5d16e3ad51701cc511b
FlutLab.io <>
2021-11-02 13:49:49 +0000

    Autosave

--- lib/screens/LandingPage.dart
+++ lib/screens/LandingPage.dart
@@ -21,59 +21,57 @@ class LandingPage extends StatelessWidget {
           height: size.height,
           child: Stack(
             children: [
-              SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    addVerticalSpace(padding),
-                    Padding(
-                      padding: sidePadding,
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  addVerticalSpace(padding),
+                  Padding(
+                    padding: sidePadding,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        BorderIcon(width: 50, height: 50, child: Icon(Icons.menu, color: COLOR_BLACK)),
+                        BorderIcon(width: 50, height: 50, child: Icon(Icons.settings, color: COLOR_BLACK))
+                      ],
+                    ),
+                  ),
+                  addVerticalSpace(padding),
+                  Padding(
+                    padding: sidePadding,
+                    child: Text('City', style: themeData.textTheme.bodyText1),
+                  ),
+                  addVerticalSpace(padding),
+                  Padding(
+                    padding: sidePadding,
+                    child: Text('San Francisco', style: themeData.textTheme.headline1),
+                  ),
+                  Padding(padding: sidePadding, child: Divider(height: padding, color: COLOR_GREY)),
+                  addVerticalSpace(10),
+                  SingleChildScrollView(
+                      physics: BouncingScrollPhysics(),
+                      scrollDirection: Axis.horizontal,
                       child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          BorderIcon(width: 50, height: 50, child: Icon(Icons.menu, color: COLOR_BLACK)),
-                          BorderIcon(width: 50, height: 50, child: Icon(Icons.settings, color: COLOR_BLACK))
-                        ],
-                      ),
-                    ),
-                    addVerticalSpace(padding),
-                    Padding(
+                          "<\$220,000",
+                          "For Sale",
+                          "3-4 Beds",
+                          ">1000 sqft"
+                        ].map((filter) => ChoiceOption(text: filter)).toList(),
+                      )),
+                  addVerticalSpace(10),
+                  Expanded(
+                    child: Padding(
                       padding: sidePadding,
-                      child: Text('City', style: themeData.textTheme.bodyText1),
-                    ),
-                    addVerticalSpace(padding),
-                    Padding(
-                      padding: sidePadding,
-                      child: Text('San Francisco', style: themeData.textTheme.headline1),
-                    ),
-                    Padding(padding: sidePadding, child: Divider(height: padding, color: COLOR_GREY)),
-                    addVerticalSpace(10),
-                    SingleChildScrollView(
+                      child: ListView.builder(
                         physics: BouncingScrollPhysics(),
-                        scrollDirection: Axis.horizontal,
-                        child: Row(
-                          children: [
-                            "<\$220,000",
-                            "For Sale",
-                            "3-4 Beds",
-                            ">1000 sqft"
-                          ].map((filter) => ChoiceOption(text: filter)).toList(),
-                        )),
-                    addVerticalSpace(10),
-                    Expanded(
-                      child: Padding(
-                        padding: sidePadding,
-                        child: ListView.builder(
-                          physics: BouncingScrollPhysics(),
-                          itemCount: RE_DATA.length,
-                          itemBuilder: (context, index) {
-                            return RealEstateItem(itemData: RE_DATA[index]);
-                          },
-                        ),
+                        itemCount: RE_DATA.length,
+                        itemBuilder: (context, index) {
+                          return RealEstateItem(itemData: RE_DATA[index]);
+                        },
                       ),
-                    )
-                  ],
-                ),
+                    ),
+                  )
+                ],
               ),
               Positioned(
                 bottom: 20,
