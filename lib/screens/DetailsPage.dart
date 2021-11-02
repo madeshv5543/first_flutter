8833268a94e90a9059d98ab823a6fddc44364ac3
FlutLab.io <>
2021-11-02 14:01:24 +0000

    Autosave

--- lib/screens/DetailsPage.dart
+++ lib/screens/DetailsPage.dart
@@ -3,6 +3,7 @@ import 'package:helloworld/utils/constants.dart';
 import 'Package:helloworld/custom/BorderIcon.dart';
 import 'package:helloworld/utils/widget_functions.dart';
 import 'package:helloworld/utils/custom_functions.dart';
+import 'Package:helloworld/custom/OptionButton.dart';
 
 class DetailsPage extends StatelessWidget {
   final dynamic itemData;
@@ -20,84 +21,100 @@ class DetailsPage extends StatelessWidget {
             body: Container(
               width: size.width,
               height: size.height,
-              child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Stack(
+              child: Stack(
+                children: [
+                  SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Image.asset(itemData['image']),
-                        Positioned(
-                          top: padding,
-                          width: size.width,
-                          child: Padding(
-                            padding: sidePadding,
+                        Stack(
+                          children: [
+                            Image.asset(itemData['image']),
+                            Positioned(
+                              top: padding,
+                              width: size.width,
+                              child: Padding(
+                                padding: sidePadding,
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    InkWell(
+                                        onTap: () {
+                                          Navigator.pop(context);
+                                        },
+                                        child: BorderIcon(
+                                          width: 50,
+                                          height: 50,
+                                          child: Icon(Icons.keyboard_backspace, color: COLOR_BLACK),
+                                        )),
+                                    BorderIcon(width: 50, height: 50, child: Icon(Icons.favorite_border, color: COLOR_BLACK))
+                                  ],
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                        addVerticalSpace(padding),
+                        Padding(
+                          padding: sidePadding,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                children: [
+                                  Text("${formatcurrency(itemData['amount'])}", style: themeData.textTheme.headline1),
+                                  addVerticalSpace(5),
+                                  Text("${itemData['address']}", style: themeData.textTheme.bodyText2)
+                                ],
+                              ),
+                              BorderIcon(
+                                child: Text("20 hours ago", style: themeData.textTheme.headline5),
+                                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                              )
+                            ],
+                          ),
+                        ),
+                        addVerticalSpace(padding),
+                        Padding(
+                          padding: sidePadding,
+                          child: Text("House Information", style: themeData.textTheme.headline4),
+                        ),
+                        addVerticalSpace(padding),
+                        SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
                             child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
-                                InkWell(
-                                    onTap: () {
-                                      Navigator.pop(context);
-                                    },
-                                    child: BorderIcon(
-                                      width: 50,
-                                      height: 50,
-                                      child: Icon(Icons.keyboard_backspace, color: COLOR_BLACK),
-                                    )),
-                                BorderIcon(width: 50, height: 50, child: Icon(Icons.favorite_border, color: COLOR_BLACK))
+                                InformationTitle(content: "${itemData['area']}", name: "Squre Foot"),
+                                InformationTitle(content: "${itemData['bathrooms']}", name: 'Bathrooms'),
+                                InformationTitle(content: "${itemData['bedrooms']}", name: 'bedrooms'),
+                                InformationTitle(content: "${itemData['garage']}", name: 'Garage')
                               ],
-                            ),
+                            )),
+                        addVerticalSpace(padding),
+                        Padding(
+                          padding: sidePadding,
+                          child: Text(
+                            "${itemData['description']}",
+                            textAlign: TextAlign.justify,
+                            style: themeData.textTheme.bodyText2,
                           ),
                         )
                       ],
                     ),
-                    addVerticalSpace(padding),
-                    Padding(
-                      padding: sidePadding,
+                  ),
+                  Positioned(
+                      bottom: 20,
                       child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Column(
-                            children: [
-                              Text("${formatcurrency(itemData['amount'])}", style: themeData.textTheme.headline1),
-                              addVerticalSpace(5),
-                              Text("${itemData['address']}", style: themeData.textTheme.bodyText2)
-                            ],
+                          OptionButton(
+                            width: size.width * 0.35,
+                            icon: Icons.message,
                           ),
-                          BorderIcon(
-                            child: Text("20 hours ago", style: themeData.textTheme.headline5),
-                            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-                          )
+                          addHorizontalSpace(10),
+                          OptionButton(width: size.width * 0.35, icon: Icons.call)
                         ],
-                      ),
-                    ),
-                    addVerticalSpace(padding),
-                    Padding(
-                      padding: sidePadding,
-                      child: Text("House Information", style: themeData.textTheme.headline4),
-                    ),
-                    addVerticalSpace(padding),
-                    SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        child: Row(
-                          children: [
-                            InformationTitle(content: "${itemData['area']}", name: "Squre Foot"),
-                            InformationTitle(content: "${itemData['bathrooms']}", name: 'Bathrooms'),
-                            InformationTitle(content: "${itemData['bedrooms']}", name: 'bedrooms'),
-                            InformationTitle(content: "${itemData['garage']}", name: 'Garage')
-                          ],
-                        )),
-                    addVerticalSpace(padding),
-                    Padding(
-                      padding: sidePadding,
-                      child: Text(
-                        "${itemData['description']}",
-                        textAlign: TextAlign.justify,
-                        style: themeData.textTheme.bodyText2,
-                      ),
-                    )
-                  ],
-                ),
+                      ))
+                ],
               ),
             )));
   }
